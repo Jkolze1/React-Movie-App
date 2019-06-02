d6f95ed54833621f0012bc8e043d467c1cf0caba
Jacqueline Kolze <jkolze1@stumail.jccc.edu>
2019-06-01 22:59:01 -0500

    Updates

---
+++ precache-manifest.716bf3d3373b1335d4925a960e35ded0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09933e29b9c1450ff2ae4e0ebeecdeed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a28c43728e480713d4b1",
+    "url": "/static/css/main.0e0af98a.chunk.css"
+  },
+  {
+    "revision": "1404bfe01b89287c6b3a",
+    "url": "/static/js/2.f6fc6daa.chunk.js"
+  },
+  {
+    "revision": "a28c43728e480713d4b1",
+    "url": "/static/js/main.62d207dd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
